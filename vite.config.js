9db8b6b0ef1ba1b9fe1df0c09ec39d88a0c8035a
stblt <staniguchi@bltinc.co.jp>
2026-04-16 12:24:46 +0900

    add: alt属性

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   appType: "mpa",
   server: {
-    open: "/index_2.html",
+    open: "/index_1.html",
   },
   build: {
     rollupOptions: {
-      input: "index_2.html",
+      input: "index_1.html",
     },
   },
 });
